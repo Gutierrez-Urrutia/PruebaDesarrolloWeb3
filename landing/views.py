3e87cd9516037837122d162d77871ad6a834f79c
Pablo Gutierrez <pa.gutierrezu@duocuc.cl>
2024-06-20 10:57:53 -0400

    Se agrega paginación en landingpage para mostrar 8 comics por pagina

--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from crud.models import Comic
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
 
-    comics = Comic.objects.all()
+    comics_list = Comic.objects.all()
+    paginator = Paginator(comics_list, 8)  # Muestra 8 cómics por página
+
+    page_number = request.GET.get('page')
+    comics = paginator.get_page(page_number)
+
     return render(request, 'index.html', {'comics': comics})
