9586a29cc4c595494329257c9efbfbbeab3388b3
Pablo Gutierrez <pa.gutierrezu@duocuc.cl>
2024-06-30 13:34:10 -0400

    Se aplica clase 'form-control' a formulario de login, se agrega .order_by('id) a lista de comics, para eliminar warning de paginacion

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from .views import login_view  # Asegúrate de importar tu vista personalizada
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='login.html'), name='login')
+    path('', login_view, name='login')  # Actualiza esta línea para usar tu vista personalizada
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
 from .forms import FormLogin
+from django.contrib.auth import login
 
+def login_view(request):
+    if request.method == 'POST':
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            user = form.user
+            login(request, user)
+
+            return redirect('home')
+    else:
+        form = FormLogin()
+    return render(request, 'login.html', {'form': form})
