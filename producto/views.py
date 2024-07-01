8d846196c66a0789598cc77ec39576b3ba87d4e4
Pablo Gutierrez <pa.gutierrezu@duocuc.cl>
2024-07-01 13:29:51 -0400

    Se mejora responsibidad en producto.html, se redirecciona error 404 a index.html

--- producto/views.py
+++ producto/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from crud.models import Comic
 import logging
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
 
 logger = logging.getLogger(__name__)
 # Create your views here.
 def producto(request):
     comic_id = request.GET.get('id')
     logger.debug('comic_id: %s', comic_id)
-    comic = get_object_or_404(Comic, id=comic_id)
-    context = { 'comic': comic }
-    return render(request, 'producto.html', context)
+    try:
+        comic = Comic.objects.get(id=comic_id)
+        context = {'comic': comic}
+        return render(request, 'producto.html', context)
+    except ObjectDoesNotExist:
+        return redirect('/')
