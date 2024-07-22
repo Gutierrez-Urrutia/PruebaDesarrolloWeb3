ab492f57079ecec7edbcd66dae617b2755e3bac7
Pablo Gutierrez <pa.gutierrezu@duocuc.cl>
2024-07-22 14:04:32 -0400

    Ultimos cambios 22/07

--- landing/views.py
+++ landing/views.py
@@ -7,8 +7,8 @@ def index(request):
 
     comics_list = Comic.objects.all().order_by('id')
     paginator = Paginator(comics_list, 8)  # Muestra 8 cómics por página
-
     page_number = request.GET.get('page')
     comics = paginator.get_page(page_number)
 
-    return render(request, 'index.html', {'comics': comics})
+    context = { 'comics': comics }
+    return render(request, 'index.html', context)
